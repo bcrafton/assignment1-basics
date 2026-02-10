7d0a0afbf30573d7a1e89d1170bb6e8079723df8
Brian Crafton <bcrafton3@gatech.edu>
2026-02-10 08:01:23 -0800

    just copy AdamW; test_get_lr_cosine_schedule

--- tests/adapters.py
+++ tests/adapters.py
@@ -14,6 +14,7 @@ from cs336_basics.tokenizer import Tokenizer
 from cs336_basics import model
 
 from cs336_basics.nn_utils import cross_entropy
+from cs336_basics.optimizer import AdamW, get_lr_cosine_schedule
 
 def run_linear(
     d_in: int,
@@ -536,7 +537,7 @@ def get_adamw_cls() -> Any:
     """
     Returns a torch.optim.Optimizer that implements AdamW.
     """
-    raise NotImplementedError
+    return AdamW
 
 
 def run_get_lr_cosine_schedule(
@@ -564,7 +565,7 @@ def run_get_lr_cosine_schedule(
     Returns:
         Learning rate at the given iteration under the specified schedule.
     """
-    raise NotImplementedError
+    return get_lr_cosine_schedule(it, max_learning_rate, min_learning_rate, warmup_iters, cosine_cycle_iters)
 
 
 def run_save_checkpoint(
